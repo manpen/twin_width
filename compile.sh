53ee57789893a13752374f10badfe14ac971917b
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2023-05-24 16:11:46 +0200

    Avoid wrongful double compilation

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-zig build solver -Doptimize=ReleaseFast -Dcpu=haswell
-zig build solver_exact -Doptimize=ReleaseFast -Dcpu=haswell
+zig build -Doptimize=ReleaseFast -Dcpu=haswell
 strip zig-out/bin/solver
 strip zig-out/bin/solver_exact
